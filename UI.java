e51f7a90fd63e84199704bf22f8e650df77647c5
but202 <83202342+but202@users.noreply.github.com>
2023-04-22 20:10:43 +0700

    Update UI.java

--- UI.java
+++ UI.java
@@ -30,7 +30,7 @@ public class UI {
         GridLayout grid = new GridLayout(0,3);
 
         // Adding buttons and counters
-        JButton setup = new JButton("Choose difficulty");
+        JButton setup = new JButton("Customize");
         JButton help = new JButton("How to play");
         JButton reset = new JButton("Reset");
         JButton undo = new JButton("Undo");
